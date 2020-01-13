ff10d68d2e32ebb0264525c42ab5cd2851d31f4d
Rafał Szymanek <241436@student.pwr.edu.pl>
2020-01-13 12:16:22 +0100

    Add link to /profile in navbar as 'Moje zamowienia'

--- resources/views/_partials/navbar.blade.php
+++ resources/views/_partials/navbar.blade.php
@@ -23,6 +23,9 @@
                 {{ Auth::user()->name }} <span class="caret"></span>
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/profile">
+                    Moje zamówienia
+                </a>
                 <a class="dropdown-item" href="{{ route('logout') }}"
                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                     {{ __('Logout') }}
