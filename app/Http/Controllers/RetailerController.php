6d73ed4a3c277928b5ff177516f9ff6faad27e33
Rafał Szymanek <241436@student.pwr.edu.pl>
2020-01-15 18:00:31 +0100

    Check if user is logged in and role == RETAILER

--- app/Http/Controllers/RetailerController.php
+++ app/Http/Controllers/RetailerController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RetailerController extends Controller
 {
     public function index()
     {
-       
+        $user = Auth::user();
+        if ($user == null || !$user->role != 'RETAILER') {
+            return abort(404);
+        }
 
 
 
        
-    return view('retailer.index');
+        return view('retailer.index');
 
     }
 }
